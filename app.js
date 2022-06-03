8d90f8cd9956b5df42ea8eb3ca292c7de9807d4d
Christian Johnsson <chjohnsson@hotmail.com>
2022-06-03 15:32:38 +0200

    Menu database

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const app = express();
-const {} = require('./modules/nedb')
-
+const {menuresult} = require('./modules/nedb')
+app.use(express.json())
 
 function authenticate(){
     // GÖR EN AUTH FUNKTION
 }
 
 
-// /api/menu	GET	Returnerar en kaffemeny (se bifogad json nedanför)
+// /api/menu	GET	Returnerar en kaffemeny
 app.get('/api/menu', async (request, response)=> {
-
+const menuresults = await menuresult();
+const resObj = {menu: menuresults}
+response.json(resObj)
 })
 
 // /api/order	POST	Sparar en kaffebeställning för en användare och returnerar en ETA-tid och ordernummer 
 // (båda dessa kan slumpas) till frontend. Om ett användarnamn skickas med i beställningen ska ordern kopplas till 
 // detta användarnamn i databasen. Ifall inget användarnamn skickas med så ska beställningen sparas som gäst.
 app.post('/api/order', async (request, response)=> {
-    
+
     // new Date().toLocaleTimeString())
 })
 
@@ -37,9 +39,6 @@ app.post('/api/account/signup', async (request, response)=> {
 app.post('/api/account/login', async (request, response)=> {
 
 })
-
-
-
 
 
 app.listen(7777, ()=>{
--- modules/nedb.js
+++ modules/nedb.js
@@ -1,16 +1,17 @@
 const nedb =  require('nedb-promise')
 const userdatabase = new nedb({filename: 'userdatabase.db', autoload:true})
 const orderdatabase = new nedb({filename: 'orderdatabase.db', autoload:true})
-const menudatabase = new nedb({filename: 'menudatabase.db', autoload:true})
+const menudatabase = new nedb({filename: 'menuedatabase.db', autoload:true})
 
 
 //MENU KLAR I MENUDATABASEN
 
+function menuresult(){
+const result = menudatabase.find({})
+return result
+}
 
 
-// function firstpushmenu(){
-// const result = menudatabase.()
-// }
 
 
-module.exports = {}
+module.exports = {menuresult}
