cee850ba5114a365bb0bfddf1f52f9ef73f3c2ed
Ida Larsson <77833853+IdaLarsson90@users.noreply.github.com>
2022-06-07 12:01:07 +0200

    Update nedb.js

--- modules/nedb.js
+++ modules/nedb.js
@@ -1,37 +1,37 @@
 const nedb =  require('nedb-promise')
-const userdatabase = new nedb({filename: 'userdatabase.db', autoload:true})
-const orderdatabase = new nedb({filename: 'orderdatabase.db', autoload:true})
-const menudatabase = new nedb({filename: 'menuedatabase.db', autoload:true})
+const userDatabase = new nedb({filename: 'userdatabase.db', autoload:true})
+const orderDatabase = new nedb({filename: 'orderdatabase.db', autoload:true})
+const menuDatabase = new nedb({filename: 'menuedatabase.db', autoload:true})
 
 
 //MENU KLAR I MENUDATABASEN
 
-function menuresult(){
-const result = menudatabase.find({})
+function menuResult(){
+const result = menuDatabase.find({})
 return result
 }
 
-function checkaccount(credentials){
-    const result = userdatabase.find({ $or: [ {email: credentials.email}, {username: credentials.username} ] })
+function checkAccount(credentials){
+    const result = userDatabase.find({ $or: [ {email: credentials.email}, {username: credentials.username} ] })
     return result
 }
 
-function createaccount(credentials){
-    const result = userdatabase.insert({ email: credentials.email , username: credentials.username, password: credentials.password })
+function createAccount(credentials){
+    const result = userDatabase.insert({ email: credentials.email , username: credentials.username, password: credentials.password })
     return result
 }
 
-function loginaccount(credentials){
-    const result = userdatabase.find({$and: [{username: credentials.username}, {password: credentials.password}] })
+function loginAccount(credentials){
+    const result = userDatabase.find({$and: [{username: credentials.username}, {password: credentials.password}] })
     return result
 }
 function findOrders(credentials){
-    const result = orderdatabase.find({id: credentials })
+    const result = orderDatabase.find({id: credentials })
     return result
 }
 
-    const result = orderdatabase.insert({username: credentials.username, order: credentials.id })
-    return result
-}
+//     const result = orderDatabase.insert({username: credentials.username, order: credentials.id })
+//     return result
+// }
 
-module.exports = {menuresult, checkaccount, createaccount, loginaccount, order}
+module.exports = {menuResult, checkAccount, createAccount, loginAccount, order}
